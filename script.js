46aa16e5985959435d7e061af590543d3023afc1
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-01-14 20:37:50 -0600

    learned about other ways of filling arrays. Learned how to convert a nodelist from the frontend to a real array

--- script.js
+++ script.js
@@ -532,13 +532,47 @@ console.log(owners.sort()); // Mutates the array
 //   if (a > b) return 1;
 //   if (a < b) return -1;
 // });
-movements.sort((a, b) => a - b);
-console.log(movements);
+// movements.sort((a, b) => a - b);
+// console.log(movements);
 
 // Descending;
 // movements.sort((a, b) => {
 //   if (a > b) return -1;
 //   if (a < b) return 1;
-// });
-movements.sort((a, b) => b - a);
-console.log(movements);
+// // });
+// movements.sort((a, b) => b - a);
+// console.log(movements);
+
+///////////////////////////////////////
+// More Ways of Creating and Filling Arrays
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Emprty arrays + fill method
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5));
+x.fill(1, 3, 5);
+x.fill(1);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('$', ''))
+  );
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+});
